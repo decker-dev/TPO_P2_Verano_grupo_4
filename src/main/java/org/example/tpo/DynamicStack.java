1d6045ac3c90fc97bf29c494313dd7da4a9db4fd
Decker <decker@commet.co>
2025-02-19 21:00:06 -0300

    Add Stack interface and DynamicStack implementation with basic stack operations

---
+++ src/main/java/org/example/tpo/DynamicStack.java
@@ -0,0 +1,33 @@
+package org.example.tpo;
+
+public class DynamicStack<T> implements Stack<T> {
+
+    private Node<T> top;
+
+    @Override
+    public T getTop() {
+        if (this.isEmpty()) {
+            throw new RuntimeException("No se puede obtener el tope de una pila vacía");
+        }
+        return this.top.getValue();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return this.top == null;
+    }
+
+    @Override
+    public void add(T a) {
+        // Se agrega un nuevo nodo al inicio, el cual apunta al tope actual.
+        this.top = new Node<>(a, this.top);
+    }
+
+    @Override
+    public void remove() {
+        if (this.isEmpty()) {
+            throw new RuntimeException("No se puede desapilar de una pila vacía");
+        }
+        this.top = this.top.getNext();
+    }
+}
